0f9e1095ccec66d226eedccb904a55586a8f1ed1
Farokhlagha <fl.razeghi@gmail.com>
2024-02-10 19:58:17 +0000

    jhjk

---
+++ SQLQuery7.sql
@@ -0,0 +1,28 @@
+Create table #temp_employee2 (
+EmployeeID int,
+JobTitle varchar(100),
+Salary int
+)
+
+Select * From #temp_employee2
+
+Insert into #temp_employee2 values (
+'1001', 'HR', '45000'
+)
+
+Insert into #temp_employee2 
+SELECT * From [SQL Tutorial]..EmployeeSalary
+
+Select * From #temp_employee2
+
+
+
+
+DROP TABLE IF EXISTS #temp_employee3
+Create table #temp_employee3 (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
+
