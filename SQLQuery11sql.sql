8ad2bb26b7a04e9a21c6f308a88db45233ec895e
Farokhlagha <fl.razeghi@gmail.com>
2024-02-21 10:40:00 +0000

    sql11

--- SQLQuery11sql.sql
+++ SQLQuery11sql.sql
@@ -1,57 +1,41 @@
-TRUNCATE TABLE	[Database1].dbo.Example
+Create table #temp_employee2 (
+EmployeeID int,
+JobTitle varchar(100),
+Salary int
+)
+
+Select * From #temp_employee2
+
+Insert into #temp_employee2 values (
+'1001', 'HR', '45000'
+)
+
+Insert into #temp_employee2 
+SELECT * From SQLTutorial..EmployeeSalary
+
+Select * From #temp_employee2
 
 
 
 
-INSERT INTO Scorecard.[dbo].Example
-SELECT  DISTINCT  Masterkey
-FROM	[AggregateDatabase1].dbo.[Example_Table]
-WHERE	IDOrganization = 279
-AND Masterkey LIKE '292%'
+DROP TABLE IF EXISTS #temp_employee3
+Create table #temp_employee3 (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
 
 
+Insert into #temp_employee3
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM SQLTutorial..EmployeeDemographics emp
+JOIN SQLTutorial..EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+group by JobTitle
 
+Select * 
+From #temp_employee3
 
-
--- Getting patients first, last, and DOB
-
-
-UPDATE Example
-SET	[Patient's First Name]	= Pers..PsnFirst,	[Patient's Late Name]	=  Pers.PsnLaSt,	[Patient's Date of Birth]	=  Pers.Psnous,	[Client Name]	= Pers.PcPPracticeName
-FROM [AggregateDatabase1].dbo.[Example_Table] Pers
-JOIN  Scorecard.Example  samp
-ON  Pers.MasterKey	=   samp.MRN
-WHERE Pers.IDOrganization = Pers.IDMasterOrganization
-AND  Pers.Idstatus = 1
-
-
-
-
---  getting most recent phone number
-
-UPDATE  Example
-SET	[Patient's  Phone  Number]	=  replace(replace(replace(replace(replace(replace(replace(replace(LTRIM(RTRIM(A.Phone)),'-',''),'(',''),')',''), ' ' ,''), '/' ,''),'x',''),'*',''),'calls','')
-FROM (
- 
-SELECT  Phone [MasterKey],Phone.Phone,Phone.DateUpdated, ROW_NUMBER()  OVER (PARTITION BY Phone.MasterKey ORDER BY Phone.DateUpdated DESC) RN
-FROM  [AggregateDatabase1].dbo.[Phone_Table]   Phone 
-	INNER  JOIN  Scorecard.Example samp
-ON Phone.Masterkey = samp.MRN
-WHERE  PhoneType  in ('CELL','HOME') 
-and  ISNULL ([Phone],'')  <> ''
-and phone not  like  '%[a-z]%'
-and  replace(replace(replace(replace(replace(replace(replace(replace(LTRIM(RTRIM(Phone.Phone)),'-',''),'(',''),')',''), ' ' ,''), '/' ,''),'x',''),'*',''),'calls','') NOT LIKE 'e%'
-and lenreplace(replace(replace(replace(replace(replace(replace(replace(LTRIM(RTRIM(Phone.Phone)),'-',''),'(',''),')',''), ' ' ,''), '/' ,''),'x',''),'*',''),'calls','') = 10
-) A
-
-join [AggregateDatabase1].dbo.[Person_Table] j 	on a.MasterKey = j.MasterKey
-join [AggregateDatabase1].dbo.[Master_Patient_Index_Table]  d  on  j.IDOrganization =  d.IDOrganization  END J.IDPerson = d.IDPerson 
-INNER JOIN Scorecard.dbo.example Outp
-ON A.MasterKey = OutP.MRN
-WHERE  RN = 1
-
-
-
-
-
---  formats  phone  number
+SELECT AvgAge * AvgSalary
+from #temp_employee3
