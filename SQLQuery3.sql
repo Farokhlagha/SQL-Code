744b25b873df68edbdd19761aa77d04bac5f5ee6
Farokhlagha <fl.razeghi@gmail.com>
2024-02-23 09:24:50 +0000

    gfh

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -13,3 +13,9 @@ Age int,
 Gender varchar(50)
 )
 
+Table 3 Insert:
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female')
